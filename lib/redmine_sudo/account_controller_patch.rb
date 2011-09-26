567118a4fa59ad2e936206da570f34956ed09036
Alex Shulgin <ash@commandprompt.com>
2011-09-26 09:48:48 +0300

    Added sudo expiration (15 min) and flash notices.

--- lib/redmine_sudo/account_controller_patch.rb
+++ lib/redmine_sudo/account_controller_patch.rb
@@ -1,14 +1,20 @@
 module RedmineSudo
   module AccountControllerPatch
     def login_sudo
-      User.current.sudo = session[:sudo] = true if User.current.has_sudo?
+      if User.current.has_sudo?
+        User.current.sudo = session[:sudo] = true
+        session[:sudo_expires] = Time.now + 15.minutes
+      end
+      flash[:notice] = l(:notice_sudo_logged_in)
       redirect_to :back
     rescue RedirectBackError
       redirect_to home_path
     end
 
     def logout_sudo
-      session[:sudo] = false
+      session.delete(:sudo)
+      session.delete(:sudo_expires)
+      flash[:notice] = l(:notice_sudo_logged_out)
       redirect_to :back
     rescue RedirectBackError
       redirect_to home_path
--- lib/redmine_sudo/application_controller_patch.rb
+++ lib/redmine_sudo/application_controller_patch.rb
@@ -5,8 +5,17 @@ module RedmineSudo
     end
 
     def find_current_user_with_sudo
+      sudo = session[:sudo]
+      sudo_expires = sudo.blank? ? (session.delete(:sudo_expires); nil) :
+        session[:sudo_expires]
+
+      if sudo_expires.blank? || Time.now > sudo_expires
+        session.delete(:sudo)
+        session.delete(:sudo_expires)
+      end
+
       find_current_user_without_sudo.tap do |user|
-        user.sudo = true if user && session[:sudo]
+        user.sudo = session[:sudo] if user
       end
     end
   end
